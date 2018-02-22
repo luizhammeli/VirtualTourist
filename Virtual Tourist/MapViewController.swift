1a2125f040cc77c849264c5a42d1d1d71b6fdd4d
Luiz Hammerli <luiz@whitebook.com.br>
2018-02-22 15:27:22 -0300

    first commit

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  Virtual Tourist
 //
 //  Created by iOS Developer on 22/02/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
